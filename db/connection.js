2ee73a75484e40727a9515b39b3d9eec882ead9d
Dezerey Escanuelas <dezerey11@gmail.com>
2021-05-19 18:07:07 -0700

    first commit

---
+++ db/connection.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+const { MONGODB_URL } = process.env;
+
+//connect
+mongoose.connect(MONGODB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+//messages
+mongoose.connection
+  .on("open", () => console.log("connected to mongo"))
+  .on("close", () => console.log("disconnected to mongo"))
+  .on("error", (error) => console.log(error));
+
+module.exports.mongoose;
