32a760e43c0e7ff120c94eaebf082fb68bf8ac0a
Dezerey Escanuelas <dezerey11@gmail.com>
2021-05-25 00:00:04 -0700

    adds user auth

--- server.js
+++ server.js
@@ -5,7 +5,19 @@ const morgan = require("morgan");
 const cors = require("cors");
 const app = express();
 const { PORT = 4000 } = process.env;
+const AuthRouter = require("./controllers/user");
+const auth = require("./auth");
 
-app.get("/", (req, res) => res.send("hello world"));
+// Middleware
+app.use(express.json());
+app.use(cors());
+app.use(morgan("tiny"));
 
+// Routers
+app.get("/", auth, (req, res) => {
+  res.json(req.payload);
+});
+app.use("/auth", AuthRouter);
+
+// Listener
 app.listen(PORT, () => console.log(`port running on ${PORT}`));
